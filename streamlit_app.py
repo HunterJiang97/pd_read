503f9955098b0d26b32532e7a1e5d8e399711fe0
Hunter <50696224+HunterJiang97@users.noreply.github.com>
2024-08-22 20:44:57 +0000

    optimized

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,23 +31,40 @@ option = st.selectbox(
 if st.button("Check this File"):
     st.session_state.f_name = option
     st.session_state.df = pd.read_csv("data/{}".format(option))
+    st.session_state.df["status"] = None
     #st.dataframe(st.session_state.df)
     st.session_state.read = True
 
 st.divider()
 if st.session_state.read:
-    if st.session_state.idx < 10:#len(st.session_state.df):
+    if st.session_state.idx < len(st.session_state.df):
         st.markdown("### {}".format(st.session_state.df.at[st.session_state.idx, "title"]))
         st.markdown("{}".format(st.session_state.df.at[st.session_state.idx, "abstract"]))
+        st.write(st.session_state.df.at[st.session_state.idx, "status"])
 
-        if st.button("Pass"):
-            next_one("P")
-        if st.button("Relavent"):
-            next_one("R")
-        if st.button("Interested"):
-            next_one("I")
-        if st.button("Save"):
-            st.session_state.df.to_csv("data_marked/{}".format(st.session_state.f_name))
+        col1, col2, col3, col4, col5 = st.columns(5)
+        with col1:
+            if st.button("Pass"):
+                next_one("P")
+        with col2:
+            if st.button("Relavent"):
+                next_one("R")
+        with col3:
+            if st.button("Interested"):
+                next_one("I")
+        with col4:
+            if st.button("Go to last one"):
+                st.session_state.idx -= 1
+        with col5:
+            if st.button("Save"):
+                st.session_state.df.to_csv("data_marked/{}".format(st.session_state.f_name))
+                csv = convert_df(st.session_state.df)
+                st.download_button(
+                    label="Download current file as CSV",
+                    data=csv,
+                    file_name="tmp_checked_{}".format(st.session_state.f_name),
+                    mime="text/csv",
+                )
     else:
         st.write("Finished Task")
         st.dataframe(st.session_state.df)
